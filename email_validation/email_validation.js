a7156b502455a86b2282f71fb7f9d9354d8813e2
hyunbin1 <kimgusqls1@gmail.com>
2021-01-24 20:29:53 +0900

    complete email_validation

--- email_validation/email_validation.js
+++ email_validation/email_validation.js
@@ -5,11 +5,23 @@ function validation() {
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
     if (email.match(pattern)) {
+        form.classList.add("valid");
         form.classList.remove("invalid");
+        text.innerHTML = "Your Email Address is Valid.";
+        text.style.color = "#00ff00";
     }
     else {
         form.classList.remove("valid");
         form.classList.add("invalid");
-
+        // 틀리면 색상과 텍스트 나오게함
+        text.innerHTML = "Please Enter Valid Email Address";
+        text.style.color = "#ff0000";
+    }
+    if (email == "") {
+        form.classList.remove("valid");
+        form.classList.remove("invalid");
+        // 틀리면 색상과 텍스트 나오게함
+        text.innerHTML = "";
+        text.style.color = "#ff0000";
     }
 }
