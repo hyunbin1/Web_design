bac266bf1e4723a3b4a3043805f1173f42d5663a
hyunbin1 <kimgusqls1@gmail.com>
2021-02-01 12:14:10 +0900

    node.js Learn

---
+++ Website with JS/index.js
@@ -0,0 +1,17 @@
+let http = require('http');
+let fs = require('fs');
+let app = http.createServer(function (request, response) {
+    let url = request.url;
+    if (request.url == '/') {
+        url = '/index.html';
+    }
+    if (request.url == '/favicon.ico') {
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+
+});
+app.listen(3000);
